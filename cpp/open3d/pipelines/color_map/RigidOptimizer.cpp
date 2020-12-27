b32ae4a486b649f06cdf2e3fe5f0005f060f1128
Yixing Lao <yixing.lao@gmail.com>
2020-12-27 13:49:06 -0800

    rename

--- cpp/open3d/pipelines/color_map/RigidOptimizer.cpp
+++ cpp/open3d/pipelines/color_map/RigidOptimizer.cpp
@@ -89,51 +89,48 @@ std::shared_ptr<geometry::TriangleMesh> RunRigidOptimizer(
         const std::vector<std::shared_ptr<geometry::RGBDImage>>& images_rgbd,
         const camera::PinholeCameraTrajectory& camera_trajectory,
         const RigidOptimizerOption& option) {
-    std::shared_ptr<geometry::TriangleMesh> optimized_mesh_;
-    std::vector<std::shared_ptr<geometry::RGBDImage>> images_rgbd_;
+    std::shared_ptr<geometry::TriangleMesh> optimized_mesh;
     std::shared_ptr<camera::PinholeCameraTrajectory> camera_trajectory_;
-    std::vector<std::shared_ptr<geometry::Image>> images_gray_;
-    std::vector<std::shared_ptr<geometry::Image>> images_dx_;
-    std::vector<std::shared_ptr<geometry::Image>> images_dy_;
-    std::vector<std::shared_ptr<geometry::Image>> images_color_;
-    std::vector<std::shared_ptr<geometry::Image>> images_depth_;
+    std::vector<std::shared_ptr<geometry::Image>> images_gray;
+    std::vector<std::shared_ptr<geometry::Image>> images_dx;
+    std::vector<std::shared_ptr<geometry::Image>> images_dy;
+    std::vector<std::shared_ptr<geometry::Image>> images_color;
+    std::vector<std::shared_ptr<geometry::Image>> images_depth;
+    std::vector<std::shared_ptr<geometry::Image>> images_mask;
+    std::vector<std::vector<int>> visibility_vertex_to_image;
+    std::vector<std::vector<int>> visibility_image_to_vertex;
 
-    optimized_mesh_ = std::make_shared<geometry::TriangleMesh>(mesh);
-    images_rgbd_ = images_rgbd;
+    optimized_mesh = std::make_shared<geometry::TriangleMesh>(mesh);
     camera_trajectory_ = std::make_shared<camera::PinholeCameraTrajectory>(
             camera_trajectory);
 
-    // images_gray_, images_dx_, images_dy_, images_color_, images_depth_
+    // images_gray, images_dx, images_dy, images_color, images_depth
     // remain unachanged through out the optimizations.
     utility::LogDebug("[ColorMapOptimization] :: CreateGradientImages");
-    for (size_t i = 0; i < images_rgbd_.size(); i++) {
-        auto gray_image = images_rgbd_[i]->color_.CreateFloatImage();
+    for (size_t i = 0; i < images_rgbd.size(); i++) {
+        auto gray_image = images_rgbd[i]->color_.CreateFloatImage();
         auto gray_image_filtered =
                 gray_image->Filter(geometry::Image::FilterType::Gaussian3);
-        images_gray_.push_back(gray_image_filtered);
-        images_dx_.push_back(gray_image_filtered->Filter(
+        images_gray.push_back(gray_image_filtered);
+        images_dx.push_back(gray_image_filtered->Filter(
                 geometry::Image::FilterType::Sobel3Dx));
-        images_dy_.push_back(gray_image_filtered->Filter(
+        images_dy.push_back(gray_image_filtered->Filter(
                 geometry::Image::FilterType::Sobel3Dy));
-        auto color = std::make_shared<geometry::Image>(images_rgbd_[i]->color_);
-        auto depth = std::make_shared<geometry::Image>(images_rgbd_[i]->depth_);
-        images_color_.push_back(color);
-        images_depth_.push_back(depth);
+        auto color = std::make_shared<geometry::Image>(images_rgbd[i]->color_);
+        auto depth = std::make_shared<geometry::Image>(images_rgbd[i]->depth_);
+        images_color.push_back(color);
+        images_depth.push_back(depth);
     }
 
     utility::LogDebug("[ColorMapOptimization] :: MakingMasks");
-    std::vector<std::shared_ptr<geometry::Image>> images_mask =
-            CreateDepthBoundaryMasks(
-                    images_depth_,
-                    option.depth_threshold_for_discontinuity_check_,
-                    option.half_dilation_kernel_size_for_discontinuity_map_);
+    images_mask = CreateDepthBoundaryMasks(
+            images_depth, option.depth_threshold_for_discontinuity_check_,
+            option.half_dilation_kernel_size_for_discontinuity_map_);
 
     utility::LogDebug("[ColorMapOptimization] :: VisibilityCheck");
-    std::vector<std::vector<int>> visibility_vertex_to_image;
-    std::vector<std::vector<int>> visibility_image_to_vertex;
     std::tie(visibility_vertex_to_image, visibility_image_to_vertex) =
             CreateVertexAndImageVisibility(
-                    *optimized_mesh_, images_depth_, images_mask,
+                    *optimized_mesh, images_depth, images_mask,
                     *camera_trajectory_, option.maximum_allowable_depth_,
                     option.depth_threshold_for_visibility_check_);
 
@@ -141,7 +138,7 @@ std::shared_ptr<geometry::TriangleMesh> RunRigidOptimizer(
     std::vector<double> proxy_intensity;
     int total_num_ = 0;
     int n_camera = int(camera_trajectory_->parameters_.size());
-    SetProxyIntensityForVertex(*optimized_mesh_, images_gray_, utility::nullopt,
+    SetProxyIntensityForVertex(*optimized_mesh, images_gray, utility::nullopt,
                                *camera_trajectory_, visibility_vertex_to_image,
                                proxy_intensity, option.image_boundary_margin_);
     for (int itr = 0; itr < option.maximum_iteration_; itr++) {
@@ -166,8 +163,8 @@ std::shared_ptr<geometry::TriangleMesh> RunRigidOptimizer(
                                 double& w) {
                 w = 1.0;  // Dummy.
                 ComputeJacobianAndResidualRigid(
-                        i, J_r, r, w, *optimized_mesh_, proxy_intensity,
-                        images_gray_[c], images_dx_[c], images_dy_[c], intr,
+                        i, J_r, r, w, *optimized_mesh, proxy_intensity,
+                        images_gray[c], images_dx[c], images_dy[c], intr,
                         extrinsic, visibility_image_to_vertex[c],
                         option.image_boundary_margin_);
             };
@@ -196,19 +193,19 @@ std::shared_ptr<geometry::TriangleMesh> RunRigidOptimizer(
         }
         utility::LogDebug("Residual error : {:.6f} (avg : {:.6f})", residual,
                           residual / total_num_);
-        SetProxyIntensityForVertex(*optimized_mesh_, images_gray_,
+        SetProxyIntensityForVertex(*optimized_mesh, images_gray,
                                    utility::nullopt, *camera_trajectory_,
                                    visibility_vertex_to_image, proxy_intensity,
                                    option.image_boundary_margin_);
     }
 
     utility::LogDebug("[ColorMapOptimization] :: Set Mesh Color");
-    SetGeometryColorAverage(*optimized_mesh_, images_color_, utility::nullopt,
+    SetGeometryColorAverage(*optimized_mesh, images_color, utility::nullopt,
                             *camera_trajectory_, visibility_vertex_to_image,
                             option.image_boundary_margin_,
                             option.invisible_vertex_color_knn_);
 
-    return optimized_mesh_;
+    return optimized_mesh;
 }
 
 }  // namespace color_map
